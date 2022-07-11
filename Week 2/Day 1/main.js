697af5e09eebbcccd154fc80de079ddccc7bdf63
SamikshaTantak <samikshatantak44@gmail.com>
2022-07-11 16:53:06 +0530

    new file:

---
+++ Week 2/Day 1/main.js
@@ -0,0 +1,23 @@
+let email;
+let studentname;
+let collegename;
+let branch;
+let phoneno;
+let address;
+
+function create(){
+    
+    email = document.getElementById("email").value;
+    studentname = document.getElementById("std_name").value;
+    collegename = document.getElementById("clg_name").value;
+    branch = document.getElementById("branch").value;
+    phoneno = document.getElementById("ph_Number").value;
+    address = document.getElementById("address").value;
+
+    
+
+    document.write(email + "<br/>"+ studentname + "<br/>"+ collegename + "<br/>"+ branch + "<br/>"+ phoneno + "<br/>" +address + "<br/>" )
+
+}
+
+
